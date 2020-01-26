5c6b958e86abfc9eee70d16924ff424d9a375a4a
Mohamed <mohammed.korany.ali@gmail.com>
2020-01-26 11:37:04 +0200

    Initial Commit

---
+++ BellmanAppTaskTests/BellmanAppTaskTests.swift
@@ -0,0 +1,34 @@
+//
+//  BellmanAppTaskTests.swift
+//  BellmanAppTaskTests
+//
+//  Created by Mohamed Korany Ali on 1/26/20.
+//  Copyright © 2020 Mohamed Korany Ali. All rights reserved.
+//
+
+import XCTest
+@testable import BellmanAppTask
+
+class BellmanAppTaskTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
